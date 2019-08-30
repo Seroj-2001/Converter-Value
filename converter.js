70b5772eafbf2714055533c391ccf5944cfd69f9
Seroj-2001 <54590205+Seroj-2001@users.noreply.github.com>
2019-08-30 11:05:32 +0400

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,59 @@
+
+var usd = 488.8;
+var rub = 7.44;
+var euro = 553.05;
+var wr;
+ function q(){
+    var wr=document.getElementById("input").value*rub;
+    document.getElementById("result").value =wr;
+     
+    
+}
+ function w(){
+   var wr=document.getElementById("input").value/rub;
+    document.getElementById("result").value =wr;
+     
+    
+}
+ function e(){
+    var wr=document.getElementById("input").value*usd;
+    document.getElementById("result").value =wr;
+     
+    
+}
+ function r(){
+    var wr=document.getElementById("input").value/usd;
+    document.getElementById("result").value =wr;
+     
+    
+}
+ function t(){
+    var wr=document.getElementById("input").value*rub*usd;
+    document.getElementById("result").value =wr;
+     
+    
+}
+ function y(){
+    var wr=document.getElementById("input").value*usd*rub;
+    document.getElementById("result").value =wr;
+     
+    
+}
+function a(){
+    var wr=document.getElementById("input").value*euro;
+    document.getElementById("result").value =wr;
+     
+    
+}
+function s(){
+    var wr=document.getElementById("input").value/euro;
+    document.getElementById("result").value =wr;
+     
+}
+ 
+
+
+
+
+
+
